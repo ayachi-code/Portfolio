75a3cc5577ca1f0db29965f399ead6c758996160
Bilal ayachi <bilal.ayachi@hetmml.nl>
2021-02-20 16:39:50 +0100

    navigator laat zien waar je bent'

--- portfolio/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("specialL").style.visibility = "hidden";
-
-document.getElementById("portfoliolink").addEventListener("mouseover", (event) => {
-    document.getElementById("specialL").style.visibility = "visible";
-});
-
-document.getElementById("portfoliolink").addEventListener("mouseout", (event) => {
-    document.getElementById("specialL").style.visibility = "hidden";
-});
