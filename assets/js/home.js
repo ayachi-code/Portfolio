d4a6d46ad4727f8a9462d8495855b3a6de3dc43a
Bilal ayachi <bilal.ayachi@hetmml.nl>
2021-03-14 19:24:45 +0100

    commentaar toegevoegd

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
     setTimeout(() => {
-        $("#content").fadeIn(1200);
+        $("#content").fadeIn(700); //Na 0.7 seconde fade de knoppen in. Functie word uitgevoerd wanneer pagina geladen is.
     },1600);
 });
 
-document.getElementsByClassName("specialL")[0].style.visibility = "hidden";
+document.getElementsByClassName("specialL")[0].style.visibility = "hidden"; //De > is niet zicht baar alleen als je er over gaat hoveren.
 
-document.getElementsByClassName("portfoliolink")[0].addEventListener("mouseover", (event) => {
+document.getElementsByClassName("portfoliolink")[0].addEventListener("mouseover", (event) => { //Als er over heen word gehoverd over de >. Dam word de > niet meer onzichtbaar
     document.getElementsByClassName("specialL")[0].style.visibility = "visible";
 });
 
-
-document.getElementsByClassName("portfoliolink")[0].addEventListener("mouseout", (event) => {
+document.getElementsByClassName("portfoliolink")[0].addEventListener("mouseout", (event) => { //Hier gebeurt de omgekeerde het word onzichtbaar wanneer de muis er niet over > is
     document.getElementsByClassName("specialL")[0].style.visibility = "hidden";
 });
